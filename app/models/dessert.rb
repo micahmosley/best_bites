80e67343d267c5636dd40f811b4b52a03d066a84
micahmosley <micah.mosley@utexas.edu>
2020-08-17 17:36:52 -0500

    added validations for dessert model

--- app/models/dessert.rb
+++ app/models/dessert.rb
@@ -2,4 +2,8 @@ class Dessert < ApplicationRecord
     has_many :favorite_desserts 
     has_many :users, through: :favorite_desserts 
     has_one_attached :photo
+    validates :name, presence: true 
+    validates :recipe, presence: true
+    validates :photo, presence: true
+   
 end
