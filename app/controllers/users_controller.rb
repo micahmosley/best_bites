0f387a8f89e4968c799591602e33afce7c36e5c9
lgonzalez2 <lgonzalez_94@hotmail.com>
2020-08-17 18:15:52 -0500

    added create action from new user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,20 @@ class UsersController < ApplicationController
     end 
 
     def create 
-        binding.pry
+        @user = User.create(user_params)
+        #should add some validations in user model and check to see if the 
+        # user that was created was valid, hense actually created. If it was, 
+        # we'd redirect to user profile page or main page. If not, render new
+        # user page again with errors displayed. 
     end 
 
     def show 
+    end 
+
+    private 
+
+    def user_params 
+        params.require(:user).permit(:first_name, :last_name, :username, :password)
     end 
 end 
 
