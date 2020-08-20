a351b81ffa61518e34e522082a98cce56ea0a2a6
micahmosley <micah.mosley@utexas.edu>
2020-08-20 14:23:01 -0500

    fixed bugs in the favorite_desserts and favorite_meals controllers

--- app/controllers/favorite_desserts_controller.rb
+++ app/controllers/favorite_desserts_controller.rb
@@ -3,26 +3,25 @@ class FavoriteDessertsController < ApplicationController
     before_action :authentication_required, :current_user
 
     def create 
-       
         if params[:favorite_dessert]==nil
-            redirect_to desserts_path 
+            redirect_to user_path(@current_user)
+        else 
+            current_favorite=FavoriteDessert.find_or_create_by(user_id: session[:user_id])
+            current_favorite.dessert_id=favorite_dessert_params[:dessert_id]
+            current_favorite.save
+            redirect_to user_path(@current_user)
         end 
-        
-        current_favorite=FavoriteDessert.find_or_create_by(user_id: session[:user_id])
-        current_favorite.dessert_id=favorite_dessert_params[:dessert_id]
-        current_favorite.save
-        redirect_to desserts_path
-        
     end 
 
     def update 
-        if params[:favorite_dessert]==nil
-            redirect_to desserts_path 
+        if params[:favorite_dessert]==nil     
+            redirect_to user_path(@current_user)
+        else 
+            current_favorite=FavoriteDessert.find_by(user_id: session[:user_id])
+            current_favorite.update(favorite_dessert_params)
+            redirect_to user_path(@current_user)
         end 
         
-        current_favorite=FavoriteDessert.find_by(user_id: session[:user_id])
-        current_favorite.update(favorite_dessert_params)
-        redirect_to desserts_path
     end 
 
     private 
